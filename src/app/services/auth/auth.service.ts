fd52919c3cf2dcea873c47a7b7950b5e3dcf6db0
hyfree <wangxianqiangcn@foxmail.com>
2022-05-30 17:27:38 +0800

    增加hkdf库

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -7,6 +7,9 @@ import {HttpClient, HttpParams} from "@angular/common/http";
 import {WebsiteConfig} from "../../models/config/website-config";
 import {ConfigService} from "../config/config.service";
 import {UserToken} from "../../models/DTO/user-token";
+import {pbkdf2, pbkdf2Sync} from "crypto";
+import * as hkdf from "futoin-hkdf";
+
 
 @Injectable({
   providedIn: 'root'
@@ -45,6 +48,36 @@ export class AuthService {
     formData.set('pwd', pwd);
     let result = this.http.post<ApiRep>(url, formData);
     return result;
+
+  }
+
+  //派生密钥
+  public async keyDerivation(password: string, email: string) {
+
+    //主密钥
+    var masterPassword=password;
+    //主密钥
+    var masterKey = pbkdf2Sync(password, email, 100_000, 256, "sha512");
+    //扩展主密钥
+    var strechedMasterKey = hkdf(masterKey, 512, {salt: email, hash: 'SHA-256'});
+    //主密钥哈希
+    var masterKeyHash = pbkdf2Sync(masterKey, password, 1, 256, "sha512")
+
+    //生成key
+    let key = await crypto.subtle.generateKey({name: "AES-GCM", length: 256}, true, ["encrypt", "decrypt"]);
+    let keyRaw=  crypto.subtle.exportKey("raw",key);
+
+
+    //生成初始化向量
+    const ivBuffer = new Uint32Array(8);
+    crypto.getRandomValues(ivBuffer);
+
+
+
+
+
+
+
   }
 
 

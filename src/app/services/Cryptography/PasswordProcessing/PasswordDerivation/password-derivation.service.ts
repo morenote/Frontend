8d4a34f5d2be0957bc01bbf6de3890ddf6d0f108
hyfree-msi <wangxianqiangcn@foxmail.com>
2023-04-17 07:42:54 +0800

    替换sm-crypto加密库，支持pdkfsm3

--- src/app/services/Cryptography/PasswordProcessing/PasswordDerivation/password-derivation.service.ts
+++ src/app/services/Cryptography/PasswordProcessing/PasswordDerivation/password-derivation.service.ts
@@ -12,17 +12,25 @@ export class PasswordDerivationService implements PasswordAnswering {
   constructor() {
   }
 
+  /**
+   *
+   * @param pwd
+   * @param user
+   * @param scDto
+   * @constructor
+   */
   async TransferEncryptionIfUser(pwd: string, user: UserInfo, scDto: SecurityConfigDTO): Promise<string> {
-    //首先派生主密钥
+    //首先派生主密钥 使用PBKDF-SHA256
     let encoder = new TextEncoder();
     let passphraseKey = encoder.encode(pwd);//口令明文
     let saltBuffer = encoder.encode(user.Id);//用户id
     let cryptoKey = await window.crypto.subtle.importKey('raw', passphraseKey, {name: "PBKDF2"}, false, ['deriveBits', 'deriveKey']);
+    //备注：由于js实现的GM算法性能太低了，所以PBKDF使用的是SHA256的PBKDF2算法
     let webKey = await window.crypto.subtle.deriveKey(
       {
         "name": 'PBKDF2',
         "salt": saltBuffer,
-        "iterations": 1000000,
+        "iterations": 1_000_000,
         "hash": 'SHA-256'
       },
       cryptoKey,
@@ -32,6 +40,8 @@ export class PasswordDerivationService implements PasswordAnswering {
     );
     let exportKey = crypto.subtle.exportKey("raw", webKey);
     let masterKey = Base64.encode(pwd);
+
+
     return  masterKey;
   }
 }
